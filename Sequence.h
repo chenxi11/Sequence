7f4bd2de11f3ea4cc80a5795645bb52dc9d37769
Your Name <you@example.com>
2018-06-23 11:08:26 +0800

    add my code

---
+++ Sequence.h
@@ -0,0 +1,13 @@
+#ifndef SEQUENCE_H
+#define SEQUENCE_H
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+class Sequence{
+public:
+      Sequnence(string filename)
+      int length();
+      int numberOf(char base);
+      string longestConsecutive();
+      string longestRepeated();}
+#endif
