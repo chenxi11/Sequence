f02ff8c3548bc88993670a67885624342eb7f832
Your Name <you@example.com>
2018-06-30 09:49:08 +0800

    add my code

---
+++ Sequence.cpp
@@ -0,0 +1,22 @@
+#include"Sequence.h"
+
+
+Sequece::Sequence(string filename)
+{return 0;}
+
+int length(char*p)
+{
+   int a=0;
+   while(p[a]) 
+   a++;
+   return a;
+}
+void main(
+   char ch[1000];
+   int i;
+   cin>>ch;
+   i=length(ch);
+}
+int Sequence::numberOf(char base){return 0;}
+Sequence::string longestConsecutive(){return 0;}
+Sequence::string longestRepeated(){return 0;}
